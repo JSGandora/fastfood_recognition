f4046575f5fccf418b4a5ddd0684c088d6af3e0f
Jason Shi <jason.g.shi@gmail.com>
2016-09-02 12:10:03 +0800

    Add conclusions

--- README.md
+++ README.md
@@ -2,3 +2,45 @@
 Fast food product classification using support vector machine.
 
 Thus far the code supports clasffication of burgers, pizza, chicken, and burritos.
+
+The classification accuracy on the test set for five trials are as follows:
+
+Training set is 0.3 of total set:
+
+0.862745098039
+0.921568627451
+0.869281045752
+0.856209150327
+0.862745098039
+
+This averages to 0.874509804.
+
+Training set is 0.5 of total set:
+
+0.94495412844
+0.899082568807
+0.935779816514
+0.917431192661
+0.908256880734
+
+This averages to 0.921100917.
+
+Training set as 0.6 of the total set:
+
+0.920454545455
+0.920454545455
+0.886363636364
+0.920454545455
+0.931818181818
+
+This averages to 0.915909091.
+
+Training set as 0.75 of total set:
+
+0.927272727273
+0.927272727273
+0.963636363636
+1.0
+0.872727272727
+
+This averages to 0.938181818.
--- src/Train.py
+++ src/Train.py
@@ -3,7 +3,7 @@ import random
 
 
 def read_training_data(X, y, filename, id):
-    with open(filename,'r') as f:
+    with open(filename, 'r') as f:
         for line in f:
             x = []
             for word in line.split():
@@ -11,8 +11,9 @@ def read_training_data(X, y, filename, id):
             X.append(x)
             y.append(id)
 
-X=[]
-y=[]
+
+X = []
+y = []
 
 read_training_data(X, y, 'pizza.txt', 1)
 read_training_data(X, y, 'chicken.txt', 2)
@@ -28,4 +29,54 @@ for i in index_shuf:
     y_shuf.append(y[i])
 
 lin_clf = svm.LinearSVC()
-lin_clf.fit(X_shuf[:len(X_shuf)/2], y_shuf[:len(X_shuf)/2])
+
+import math
+train_length = int(math.floor(0.1*len(X_shuf)))
+
+lin_clf.fit(X_shuf[:train_length], y_shuf[:train_length])
+print lin_clf.score(X_shuf[train_length:], y_shuf[train_length:])
+
+# The following code is to create a confusion matrix visual
+
+from sklearn.metrics import confusion_matrix
+
+y_pred = lin_clf.predict(X_shuf[train_length:])
+y_true = y_shuf[train_length:]
+cm = confusion_matrix(y_true, y_pred)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# This code to plot the confusion marix is taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    target_names = ('Pizza', 'Chicken', 'Burger', 'Burrito')
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(4)
+    plt.xticks(tick_marks, target_names, rotation=45)
+    plt.yticks(tick_marks, target_names)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+from sklearn import svm, datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+
+print('Confusion matrix, without normalization')
+print(cm)
+plt.figure()
+plot_confusion_matrix(cm)
+
+# Normalize the confusion matrix by row (i.e by the number of samples
+# in each class)
+cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+print('Normalized confusion matrix')
+print(cm_normalized)
+plt.figure()
+plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
+
+plt.show()
